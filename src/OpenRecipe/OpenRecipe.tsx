b06aab983e8d5e591682aa19f422783128e5d7e2
Sara Szwejkowska <saraszw@op.pl>
2023-07-19 15:56:18 +0200

    single page - css&js

--- src/OpenRecipe/OpenRecipe.tsx
+++ src/OpenRecipe/OpenRecipe.tsx
@@ -4,6 +4,15 @@ import detailsRecipe from '../App';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+interface SingleRecipe {
+	id:number,
+	image: string;
+	title: string;
+	extendedIngredients: string[];
+	servings: number;
+	cuisines: string[];
+}
+
 const OpenRecipe = () => {
 	const [detailsRecipe, setDetailsRecipes] = useState<any>({});
 
@@ -13,7 +22,7 @@ const OpenRecipe = () => {
 				`https://api.spoonacular.com/recipes/${params.id}/information?apiKey=28e4f463047f47b9a250a32418c780c8`
 			)
 			.then((res) => {
-				console.log(res.data, 'C');
+				console.log(res.data);
 				setDetailsRecipes(res.data);
 			})
 			.catch((err) => {
@@ -26,14 +35,22 @@ const OpenRecipe = () => {
 	}, []);
 
 	const params = useParams();
-	console.log(params);
+	console.log(detailsRecipe);
 	return (
 		<div>
 			<div className={styles.wrapper}>
-				<span>{detailsRecipe?.title}</span>
-				<img src={detailsRecipe.image} alt="" />
-				<span>{detailsRecipe?.title}</span>
-				<span>{detailsRecipe?.title}</span>
+				<img src={detailsRecipe.image}  className= {styles.imgRecipe}alt='' />
+				<span className={styles.recipeTitle}>{detailsRecipe?.title}</span>
+ <span className={styles.ingredientsText}> Ingredients:  </span>
+				{
+				detailsRecipe.title &&
+					detailsRecipe.extendedIngredients.map((el: any, id:number) => (
+						<div key={id}>
+						<span className={styles.nameIngredient}> {el.name} </span></div>
+					))}
+					
+				<span>Kuchnia: {detailsRecipe?.cuisines}</span>
+				<span>Liczba porcji: {detailsRecipe?.servings}</span>
 			</div>
 		</div>
 	);
